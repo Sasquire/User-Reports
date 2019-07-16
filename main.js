785cb682840fc61eaeeca753ec09e2541eab4a28
Sasquire <32607148+Sasquire@users.noreply.github.com>
2019-07-16 16:20:25 -0400

    fix filtering, added blocking users

--- main.js
+++ main.js
@@ -2,14 +2,15 @@
 
 const request = require('request');
 const { JSDOM } = require('jsdom');
-const options = require('./options.json');
+const option_path = './options.json';
+const options = require(option_path);
 const fs = require('fs');
 
-main('tag_updates');
+main(options.types[0]);
 
 async function main(type){
 	const [start, end] = today_tomorrow();
-	const url = `https://e621.net/report/${type}?start_date=${start}&end_date=${end}`;
+	const url = `https://e621.net/report/${type.name}?start_date=${start}&end_date=${end}&limit=100`;
 	return new Promise((resolve, reject) => {
 		request({
 			url: url,
@@ -22,8 +23,8 @@ async function main(type){
 			} else {
 				const dom = new JSDOM(r).window.document;
 				const users = find_users(dom);
-				const filtered = filter_users(users);
-				console.log(filtered.map(p => p.name).join('\n'));
+				const filtered = filter_users(users, type.key);
+				console.log(filtered);
 			}
 		});
 	});
@@ -49,20 +50,43 @@ function find_users(dom){
 			name: e.user_link.textContent,
 			id: parseInt(e.user_link.href.match(/\d+/u)[0], 10),
 			edit_count: parseInt(e.edit_count.textContent, 10),
-			user_link: e.user_link.href,
-			edit_history_link: e.edit_count.href,
+			user_link: `https://e621.net${e.user_link.href}`,
+			edit_history_link: `https://e621.net${e.edit_count.href}`,
 			percentage: e.percentage
 		}));
 
 	return users;
 }
 
+function save_settings(json){
+	fs.writeFileSync(option_path, JSON.stringify(json, null, '\t'), 'utf8');
+}
+
+function block_user(name, type){
+	const user = options.blocked_users.find(e => e.name == name);
+	if(user){
+		user.blocked_on += type;
+	} else {
+		options.blocked_users.push({
+			name: name,
+			blocked_on: type
+		});
+	}
+
+	save_settings(options);
+}
+
 function filter_users(user_list, key){
 	// User could be blocked with this key
 	const blockable = options.blocked_users
-		.filter(e => e.blocked_on.includes(key));
+		.filter(e => e.blocked_on.includes(key))
+		.map(e => e.name);
 
-	return user_list.filter(e => !blockable.includes(e.name));
+	return user_list.filter(e => {
+		// A console.log(e.name, blockable, blockable.some(p => p == e.name))
+		// console.log(e.name, blockable.includes(e.name), key)
+		return !blockable.includes(e.name);
+	});
 }
 
 // Tomorrow is really 30 days in the future
