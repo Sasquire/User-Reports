586c0a72ceb7a1a8402022ee49c77718e2595238
Sasquire <32607148+Sasquire@users.noreply.github.com>
2019-07-16 14:40:08 -0400

    extract users

--- main.js
+++ main.js
@@ -3,10 +3,42 @@
 const request = require('request');
 const { JSDOM } = require('jsdom');
 
-request('https://e621.net/report/tag_updates', (e, h, r) => {
+request({
+	url: 'https://e621.net/report/tag_updates',
+	headers: { 'User-Agent': 'idem\'s tag edit watcher (finds bad users)' }
+}, (e, h, r) => {
 	if(e || h.statusCode != 200){
 		console.log(`Some Error ${e || h.statusCode}`);
 	} else {
-		console.log(r);
+		const dom = new JSDOM(r).window.document;
+		find_users(dom);
 	}
 });
+
+function find_users(dom){
+	const query = 'table tr.even, table tr.odd';
+	const users = Array.from(dom.querySelectorAll(query))
+		.map(e => ({
+			user_link: e.children[0].querySelector('a'),
+			edit_count: e.children[1].querySelector('a'),
+			percentage: e.children[2].textContent
+		}))
+		.filter(e => {
+			if(e.user_link === null){
+				console.log(`Other: ${e.percentage}`);
+				return false;
+			} else {
+				return true;
+			}
+		})
+		.map(e => ({
+			name: e.user_link.textContent,
+			id: parseInt(e.user_link.href.match(/\d+/u)[0], 10),
+			edit_count: parseInt(e.edit_count.textContent, 10),
+			user_link: e.user_link.href,
+			edit_history_link: e.edit_count.href,
+			percentage: e.percentage
+		}));
+
+	return users;
+}
