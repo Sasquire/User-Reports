de4f43e395f0e6f82db00a177ff4fa9415c56e93
Sasquire <32607148+Sasquire@users.noreply.github.com>
2019-07-17 17:27:30 -0400

    downloads type

--- more_active.js
+++ more_active.js
@@ -23,7 +23,7 @@ async function request(request_options){
 }
 
 
-async function download_type(type){
+async function download_type_raw(type){
 	const url = new URL(`https://e621.net/report/${type}`);
 
 	// 30 days in the future
@@ -39,6 +39,16 @@ async function download_type(type){
 	});
 }
 
+async function download_type(type){
+	const [now] = date_difference(0);
+	const raw_text = await download_type_raw(type);
+	const dom = new JSDOM(raw_text).window.document;
+	const users = find_users(dom);
+	users.forEach(e => (e.type = type));
+	users.forEach(e => (e.date = now));
+	return users;
+}
+
 // Between now and however many is specified
 function date_difference(time){
 	const now = new Date();
@@ -51,6 +61,20 @@ function date_difference(time){
 		const d_day = date.getDate().toString().padStart(2, '0');
 		return `${d_year}-${d_month}-${d_day}`;
 	}
+}
+
+function find_users(dom){
+	const query = 'table tr.even, table tr.odd';
+	const users = Array.from(dom.querySelectorAll(query))
+		.slice(0, -1) // Removes the last item
+		.map(e => ({
+			username: e.children[0].textContent,
+			user_id: parseInt(e.children[0].children[0].href.match(/\d+/u)[0], 10),
+			count: parseInt(e.children[1].textContent, 10),
+			percentage: e.children[2].textContent
+		}));
+
+	return users;
 }
 
 download_type('tag_updates').then(console.log);
